598c532b10b25f886eb9cb27bcfd1f0d2bad8133
Mimimi19 <is0677re@ed.ritsumei.ac.jp>
2024-09-18 16:32:40 +0900

    login &　register　ページを作成

---
+++ frontend/app/components/Login.tsx
@@ -0,0 +1,55 @@
+import * as React from "react";
+import { Link } from "@remix-run/react";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+export const Login = () => {
+  return (
+    <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle>ログインしてください</CardTitle>
+        <CardDescription>
+          <Link to="/account/register">
+            アカウントをお持ちでない場合はこちら
+          </Link>
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="mailAddress">メールアドレス</Label>
+              <Input id="mailAddress" />
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="framework">パスワード</Label>
+              <Input id="password" />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button variant="outline">
+          <Link to="/">Cancel</Link>
+        </Button>
+        <Button>Log In</Button>
+      </CardFooter>
+    </Card>
+  );
+};
