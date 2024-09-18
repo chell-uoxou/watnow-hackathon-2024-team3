46835259cfaba4ac467de4c624dc25485f84cefa
Mimimi19 <is0677re@ed.ritsumei.ac.jp>
2024-09-18 22:43:23 +0900

    register不要なimportの削除

--- frontend/app/components/Register.tsx
+++ frontend/app/components/Register.tsx
@@ -12,13 +12,6 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 
 export const Register = () => {
   return (
