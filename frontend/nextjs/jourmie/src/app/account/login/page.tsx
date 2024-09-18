b41faf444e52475876cc82a49bbefbc2202c86e3
chell-uoxou <chelluoxou@gmail.com>
2024-09-19 03:44:34 +0900

    feat 結構帰ってきた

---
+++ frontend/nextjs/jourmie/src/app/account/login/page.tsx
@@ -0,0 +1,5 @@
+import { Login } from "@/components/Login";
+
+export default function CardWithForm() {
+  return <Login />;
+}
---
+++ frontend/nextjs/jourmie/src/hooks/useFirebaseApp.ts
@@ -0,0 +1,11 @@
+
+import { FirebaseContext } from "@/routes/_app"
+import { useContext } from "react"
+
+export const useFirebaseApp = () => {
+    const firebaseApp = useContext(FirebaseContext)
+    if (firebaseApp === null) {
+        throw new Error('useFirebaseApp must be used within a FirebaseProvider')
+    }
+    return firebaseApp
+}
