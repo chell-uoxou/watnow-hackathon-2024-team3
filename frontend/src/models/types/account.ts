409b10232d021b738c8293aeb25f3b69393a0a60
chell-uoxou <chelluoxou@gmail.com>
2024-09-19 12:44:02 +0900

    feat Account型を例として追加

---
+++ frontend/src/models/types/account.ts
@@ -0,0 +1,10 @@
+export default interface Account {
+  email: string;
+  default_display_name: string;
+  avatar_url: string;
+  password_hash: string;
+  last_name: string;
+  first_name: string;
+  phone_number: string;
+  address: string;
+}
