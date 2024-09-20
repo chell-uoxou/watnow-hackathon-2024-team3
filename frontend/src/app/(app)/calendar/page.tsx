4316ae00b46a88514fd572bfad5b956d74d3edc1
chell-uoxou <chelluoxou@gmail.com>
2024-09-20 14:26:38 +0900

    feat イベントプール一覧を表示できるように

--- frontend/src/app/(app)/calendar/page.tsx
+++ frontend/src/app/(app)/calendar/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 import { collection, doc, Timestamp } from "firebase/firestore";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "~/components/ui/button";
 import { EventInputDialog } from "~/features/eventPool/EventInputDialog";
+import { EventPoolList } from "~/features/eventPool/EventPoolList";
 import useCurrentAccount, { isReady } from "~/hooks/useCurrentAccount";
 import { useFirestoreCollection } from "~/hooks/useFirestoreCollection";
 import { db } from "~/lib/firebase";
@@ -10,44 +11,58 @@ import { EventPool } from "~/models/types/event_pool";
 
 export default function Page() {
   const { currentDBAccount } = useCurrentAccount();
-  const currentEventPool = useFirestoreCollection<EventPool>(
+  const { list: listEventPool } = useFirestoreCollection<EventPool>(
     currentDBAccount !== "loading" && currentDBAccount?.uid
       ? collection(doc(db, "accounts", currentDBAccount.uid), "event_pool")
       : null
   );
   const [openDialog, setOpenDialog] = useState(false);
+  const [events, setEvents] = useState<EventPool[]>([]);
 
-  const handleClickAddEventPool = async () => {
-    console.log(currentDBAccount, currentEventPool);
-    if (isReady(currentDBAccount) && currentEventPool.add !== null) {
-      console.log("つくるかね");
+  // const handleClickAddEventPool = async () => {
+  //   console.log(currentDBAccount, currentEventPool);
+  //   if (isReady(currentDBAccount) && currentEventPool.add !== null) {
+  //     console.log("つくるかね");
 
-      await currentEventPool.add({
-        title: "散歩しよう",
-        description: "",
-        location: "",
-        attached_image: "",
-        available_times: [
-          {
-            start_time: Timestamp.fromDate(new Date()),
-            end_time: Timestamp.fromDate(
-              new Date(Date.now() + 4 * 60 * 60 * 1000)
-            ),
-          },
-        ],
-        default_duration: 60,
-        default_budget: {
-          mode: "per_person",
-          value: 0,
-        },
-        needs_preparation: false,
-        preparation_task: "",
-        max_participants: 1,
-        notes: "",
+  //     await currentEventPool.add({
+  //       title: "散歩しよう",
+  //       description: "",
+  //       location: "",
+  //       attached_image: "",
+  //       available_times: [
+  //         {
+  //           start_time: Timestamp.fromDate(new Date()),
+  //           end_time: Timestamp.fromDate(
+  //             new Date(Date.now() + 4 * 60 * 60 * 1000)
+  //           ),
+  //         },
+  //       ],
+  //       default_duration: 60,
+  //       default_budget: {
+  //         mode: "per_person",
+  //         value: 0,
+  //       },
+  //       needs_preparation: false,
+  //       preparation_task: "",
+  //       max_participants: 1,
+  //       notes: "",
+  //     });
+  //     console.log("つくった");
+  //   }
+  // };
+
+  useEffect(() => {
+    if (listEventPool) {
+      listEventPool().then((events) => {
+        if (events) {
+          console.log(events);
+          setEvents(events);
+        } else {
+          console.log("No events found");
+        }
       });
-      console.log("つくった");
     }
-  };
+  }, [listEventPool]);
 
   if (currentDBAccount === "loading") {
     return <div>Loading...</div>;
@@ -57,9 +72,10 @@ export default function Page() {
     return (
       <div>
         <div>Welcome {currentDBAccount.default_display_name}</div>
-        <Button onClick={handleClickAddEventPool} className="hidden">
+        {/* <Button onClick={handleClickAddEventPool} className="hidden">
           Add Event Pool
-        </Button>
+        </Button> */}
+        <EventPoolList events={events} />
         <Button onClick={() => setOpenDialog(true)}>イベント候補を追加</Button>
         <EventInputDialog isOpen={openDialog} onOpenChange={setOpenDialog} />
       </div>
