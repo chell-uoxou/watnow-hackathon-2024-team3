5024de01da98dc6d7b509ecd740b7caf2159ae72
chell-uoxou <chelluoxou@gmail.com>
2024-09-20 12:38:17 +0900

    fix defaultエクスポートになってないのを直した

--- frontend/src/app/(app)/layout.tsx
+++ frontend/src/app/(app)/layout.tsx
@@ -1,41 +1,14 @@
 import { PropsWithChildren } from "react";
 import { AppTopBar } from "~/components/common/AppTopBar";
 import { LeftSideBar } from "~/components/common/LeftSideBar";
-// import MapDrawer from "~/features/googleMap/MapDrawer";
-// import initFirebase from "~/lib/initFirebase";
 
-// export async function loader() {
-//   const config: FirebaseOptions = {
-//     apiKey: process.env.API_KEY,
-//     authDomain: process.env.AUTH_DOMAIN,
-//     projectId: process.env.PROJECT_ID,
-//     storageBucket: process.env.STORAGE_BUCKET,
-//     messagingSenderId: process.env.MESSAGING_SENDER_ID,
-//     appId: process.env.APP_ID,
-//     measurementId: process.env.MEASUREMENT_ID,
-//   };
-
-//   return json({ firebaseOptions: config });
-// }
-
-export const App = ({ children }: PropsWithChildren) => {
-  // const config = useLoaderData<typeof loader>();
-  // const firebaseApp = initFirebase(config.firebaseOptions);
+export default function App({ children }: PropsWithChildren) {
   return (
-    // <div className="bg-slate-50 h-svh">
-    //   <AppTopBar />
-    //   <LeftSideBar />
-    //   <MapDrawer />
-    //   {children}
-    // </div>
     <main className="flex flex-row min-h-screen">
       <div className="flex bg-brand-color-3 h-screen border-brand-border-color border">
         <LeftSideBar />
       </div>
-
-      {/* <MapDrawer /> */}
-
-      <div className="w-screen h-10 bg-slate-950 text-white ">
+      <div className="w-screen h-10 bg-slate-950 ">
         <AppTopBar />
         {children}
       </div>
