ab83f4899c7dfe4ace90ee2d9177165357d9cb7d
Mimimi19 <is0677re@ed.ritsumei.ac.jp>
2024-10-08 05:55:42 +0900

    グリットの色を変更

--- frontend/src/features/timeLine/components/TimelineGridLine.tsx
+++ frontend/src/features/timeLine/components/TimelineGridLine.tsx
@@ -13,7 +13,7 @@ export const TimelineGridLine = (props: TimelineGridProps) => {
   return (
     <div className="flex gap-2 w-full justify-between items-start">
       {/* 時間の設定 */}
-      <span className=" text-xs w-9">
+      <span className=" text-xs w-9 text-black">
         {/* 数字が１桁の際１０の位に０を追加 */}
         {props.hour.toString().length === 1 ? "0" + props.hour : props.hour}:
         {props.minutes === 0 ? "00" : props.minutes}
@@ -21,7 +21,7 @@ export const TimelineGridLine = (props: TimelineGridProps) => {
       {/* グリットの生成 */}
       <div
         className={
-          "flex-1 flex w-full translate-y-2 border-t-2 border-zinc-900 border-dashed"
+          "flex-1 flex w-full translate-y-2 border-t-2 border-brand-border-color border-dashed"
         }
         style={{ height }}
       />
