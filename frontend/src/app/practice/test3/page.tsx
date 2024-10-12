795650af94ef04f9ccb19beaf0f2606db72eb450
sakata <is0670pk@ed.ritsumei.ac.jp>
2024-10-12 14:33:15 +0900

    もう一回お願いします

--- frontend/src/app/practice/test3/page.tsx
+++ frontend/src/app/practice/test3/page.tsx
@@ -1,17 +1,10 @@
 "use client";
 
 import { useState } from "react";
-import Dateswitcher  from "~/features/timeline/DateSwitcher";
+import DateSwitcher  from "~/features/timeline/DateSwitcher";
 
 export default function Page() {  
   const [value, setValue] = useState(new Date());
-  const handlePreviousDay = () => {
-    setValue(new Date(value.setDate(value.getDate() - 1)));
-  };
 
-  const handleNextDay = () => {
-    setValue(new Date(value.setDate(value.getDate() + 1)));
-  };
-
-  return <Dateswitcher value={value} handlePreviousDay={handlePreviousDay} handleNextDay={handleNextDay}/>;
+  return <DateSwitcher value={value} onChange={(date) => setValue(date)}/>;
 }
--- frontend/src/features/timeline/DateSwitcher.tsx
+++ frontend/src/features/timeline/DateSwitcher.tsx
@@ -1,45 +1,75 @@
+"use client";
 import React from "react";
 import { ChevronUp } from "lucide-react";
 import { ChevronDown } from "lucide-react";
 import { Button } from "~/components/ui/button";
+import { on } from "stream";
 
-const Dateswitcher =(props) =>{
+interface DateSwitcherProps {
+  value: Date;
+  onChange: (value: Date) => void;
+}
+
+const DateSwitcher = (props: DateSwitcherProps) => {
+  const handlePreviousDay = () => {
+    const newValue = new Date(props.value);
+    newValue.setDate(newValue.getDate() - 1);
+    props.onChange(newValue);
+  };
+
+  const handleNextDay = () => {
+    const newValue = new Date(props.value);
+    newValue.setDate(newValue.getDate() + 1);
+    props.onChange(newValue);
+  };
 
   const weekDay: {
     [key: number]: string;
   } = {
-    0: '日',
-    1: '月',
-    2: '火',
-    3: '水',
-    4: '木',
-    5: '金',
-    6: '土',
+    0: "日",
+    1: "月",
+    2: "火",
+    3: "水",
+    4: "木",
+    5: "金",
+    6: "土",
   };
 
   return (
-    <div className="w-[44px] p-2">
+    <div className="w-[44px] p-2 bg-white border border-brand-border-color rounded-lg">
       <div className="flex flex-col items-center gap-2">
-        <div className="">
-          <Button variant="outline" size="icon" className="size-6" onClick={props.handlePreviousDay}>
+        <div>
+          <Button
+            variant="outline"
+            size="icon"
+            className="size-6"
+            onClick={handlePreviousDay}
+          >
             <ChevronUp className="h-4 w-4" />
           </Button>
         </div>
-        <div className="flex flex-col items-center h-[46px]">
-          <p className="font-bold text-sm h-[17px]">{props.value.getMonth() + 1}</p>
-          <p className="font-bold text-2xl h-[29px]">{props.value.getDate()}</p>
+        <div className="flex flex-col items-center">
+          <p className="font-bold text-sm">
+            {props.value.getMonth() + 1}
+          </p>
+          <p className="font-bold text-2xl">{props.value.getDate()}</p>
         </div>
         <div className="flex flex-col items-center">
           <p className="font-bold">{weekDay[props.value.getDay()]}</p>
         </div>
-        <div className="">
-          <Button variant="outline" size="icon" className="size-6" onClick={props.handleNextDay}>
+        <div>
+          <Button
+            variant="outline"
+            size="icon"
+            className="size-6"
+            onClick={handleNextDay}
+          >
             <ChevronDown className="h-4 w-4" />
           </Button>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default Dateswitcher;
+export default DateSwitcher;
