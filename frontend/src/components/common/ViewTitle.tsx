58aa8b1b29b0772f661d972fe71e0d0caa009f05
Mimimi19 <is0677re@ed.ritsumei.ac.jp>
2024-10-12 18:01:12 +0900

    view titleの”の”の削除

--- frontend/src/components/common/ViewTitle.tsx
+++ frontend/src/components/common/ViewTitle.tsx
@@ -8,7 +8,7 @@ export const ViewTitle = (props: TitleProps) => {
     <div className="flex-1 flex flex-col gap-1.5">
       {/* subtitleがある場合のみ表示 */}
       {props.subTitle && (
-        <span className="text-base">{props.subTitle + "の"}</span>
+        <span className="text-base">{props.subTitle}</span>
       )}{" "}
       <span className="text-4xl font-black">{props.title}</span>
     </div>
