a94fdb92c5e8f2edb0ad12a76cbcb023a6284692
chell-uoxou <chelluoxou@gmail.com>
2024-10-13 00:05:46 +0900

    feat プライベートカレンダーページを作成

---
+++ frontend/src/app/(app)/(calendar)/calendar/page.tsx
@@ -0,0 +1,12 @@
+import { CardBodyWithLeftSidebar } from "~/features/appLayout/CardBodyWithLeftSidebar";
+import { ViewerLeftSideBar } from "~/features/leftSideBar/ViewerLeftSidebar";
+
+export default function Page() {
+  return (
+    <CardBodyWithLeftSidebar
+      leftSidebar={<ViewerLeftSideBar title="カレンダー" subTitle="あなたの" />}
+    >
+      <div>カレンダー</div>
+    </CardBodyWithLeftSidebar>
+  );
+}
