4e2d212f03ed3a8dea16bca9ad55b00b1856f47e
Mimimi19 <is0677re@ed.ritsumei.ac.jp>
2024-10-14 19:52:33 +0900

    SearchBox.tsxをshadcn/uiに統一中途半端なコードを消してただのハリボテに直した

--- frontend/src/features/googleMap/components/SearchBox.tsx
+++ frontend/src/features/googleMap/components/SearchBox.tsx
@@ -1,31 +1,19 @@
-// SearchBox.tsx
 "use client";
-import React, { useRef } from "react";
-import { StandaloneSearchBox } from "@react-google-maps/api";
+import { Input } from "~/components/ui/input";
+import { Button } from "~/components/ui/button";
 
 interface SearchBoxProps {
   onPlacesChanged: () => void; // 場所が変更された時のイベントハンドラ
   onLoad: (ref: google.maps.places.SearchBox) => void; // SearchBoxがロードされた時のイベントハンドラ
 }
 
-const SearchBox: React.FC<SearchBoxProps> = ({ onPlacesChanged, onLoad }) => {
-  const searchBoxRef = useRef<google.maps.places.SearchBox | null>(null); // SearchBoxの参照を保持するためのuseRef
-
+const SearchBox: React.FC<SearchBoxProps> = () => {
   return (
     <div className="flex z-10">
-      <StandaloneSearchBox
-        onLoad={(ref) => {
-          searchBoxRef.current = ref; // SearchBoxのインスタンスを保存
-          onLoad(ref); // 親コンポーネントのonLoadを呼び出す
-        }}
-        onPlacesChanged={onPlacesChanged} // onPlacesChangedプロパティに渡されたハンドラを使用する
-      >
-        <input
-          type="text"
-          placeholder="地名を検索" // 検索ボックスのプレースホルダ
-          className="w-full p-2 rounded-lg border border-gray-300" // 検索ボックスのスタイル
-        />
-      </StandaloneSearchBox>
+      <div className="flex w-full max-w-sm items-center space-x-2">
+        <Input type="text" placeholder="地名を検索" />
+        <Button type="submit">検索</Button>
+      </div>
     </div>
   );
 };
