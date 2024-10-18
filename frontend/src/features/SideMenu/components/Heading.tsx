69d8ce479a1d2a2280286471811df729bb962754
sakata <is0670pk@ed.ritsumei.ac.jp>
2024-10-19 03:11:50 +0900

    必要なコンポーネントの作成

---
+++ frontend/src/features/SideMenu/components/Heading.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface HeadingProps {
+  title: string;
+}
+
+const Heading = (props: HeadingProps) => {
+  return (
+    <div className="flex p-2 items-center">
+      <h1 className=" font-bold text-xs">{props.title}</h1>
+    </div>
+  );
+};
+
+export default Heading;
