33f894ed88bc1c45ff88672fc6a08c926174667b
chell-uoxou <chelluoxou@gmail.com>
2024-10-18 21:59:10 +0900

    breaking! groupにまつわるDBのスキーマに、`created_by_account`, `created_by_group` を追加

--- frontend/src/models/types/group.ts
+++ frontend/src/models/types/group.ts
@@ -1,3 +1,7 @@
+import { DocumentReference } from "firebase/firestore";
+import { Account } from "./account";
+import { Member } from "./member";
+
 /**
  * ## Group
  * 予定を共同編集するためのグループを表す型
@@ -12,4 +16,6 @@ export type Group = {
   name: string;
   description: string;
   icon_url: string;
+  crated_by_account: DocumentReference<Account>;
+  created_by_member: DocumentReference<Member>;
 };
