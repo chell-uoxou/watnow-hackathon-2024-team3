ae59af2ea7168b8294dd1726d3e920ea8a3419f2
sakata <is0670pk@ed.ritsumei.ac.jp>
2024-10-19 04:41:41 +0900

    仮完成

--- frontend/src/features/SideMenu/SideMenu.tsx
+++ frontend/src/features/SideMenu/SideMenu.tsx
@@ -13,22 +13,21 @@ import { List } from "lucide-react";
 import { Blocks } from "lucide-react";
 import { Calendar } from "lucide-react";
 import { StickyNote } from "lucide-react";
-// import useGroupRouter from "~/hooks/useGroupRouter";
 import useCurrentGroup from "~/hooks/useCurrentGroup";
 import { LoadingSpinner } from "~/components/ui/spinner";
-
+import useGroupRouter from "~/hooks/useGroupRouter";
 
 function SideMenu() {
-  // const { isInGroup } = useGroupRouter();
+  const { isInGroup } = useGroupRouter();
   const dbGroup = useCurrentGroup();
   return (
     <div>
-      {dbGroup && (
+      {isInGroup && (
         <>
           {dbGroup === "loading" ? (
             <LoadingSpinner className="size-4 m-2" />
           ) : (
-            <Heading title={dbGroup.name.toUpperCase()} />
+            <Heading title={dbGroup?.name.toUpperCase() ?? "no group found!"} />
           )}
           <SideMenuItemWithIcon
             icon={<Flower2 className="size-5" />}
@@ -63,11 +62,13 @@ function SideMenu() {
           <Border />
         </>
       )}
+
       <Heading title="あなた" />
       <SideMenuItemWithIcon
         icon={<UserRound className="size-5" />}
         title="個人のカレンダー"
       />
+
       <SideMenuItemWithIcon
         icon={<List className="size-5" />}
         title="参加しているグループ"
