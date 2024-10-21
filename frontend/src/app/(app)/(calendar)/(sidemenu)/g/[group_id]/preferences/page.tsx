610928f8513f72ea7406bd6f24d25f4814050304
sakata <is0670pk@ed.ritsumei.ac.jp>
2024-10-21 22:36:23 +0900

    概要UI綺麗にしました

--- frontend/src/app/(app)/(calendar)/(sidemenu)/g/[group_id]/preferences/page.tsx
+++ frontend/src/app/(app)/(calendar)/(sidemenu)/g/[group_id]/preferences/page.tsx
@@ -1,11 +1,24 @@
 import React from "react";
+import Image from "next/image";
 
 function Page() {
   return (
-    <div className="flex justify-center">
-      <div>
+    <div className="flex w-full justify-center items-center">
+      <div className="flex flex-col gap-5 max-w-lg">
         <div className="font-bold text-xl ">概要</div>
-        <div></div>
+        <div className="flex justify-center">
+          <Image
+            src={"/images/defaultIcon.png"}
+            alt="Selected Icon"
+            width={100}
+            height={100}
+            className="rounded-full"
+          />
+        </div>
+        <div className="font-bold text-2xl flex justify-center">グループ名</div>
+        <div className="flex justify-center text-justify">
+          これはグループの説明を表示します。これはグループの説明を表示します。これはグループの説明を表示します。これはグループの説明を表示します。これはグループの説明を表示します。これはグループの説明を表示します。
+        </div>
       </div>
     </div>
   );
