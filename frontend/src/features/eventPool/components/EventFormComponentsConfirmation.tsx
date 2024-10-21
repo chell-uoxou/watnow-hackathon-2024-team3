ce92eb8ed47c3d28cb98b9f5395817582bc7a28f
Mimimi19 <is0677re@ed.ritsumei.ac.jp>
2024-10-21 21:35:10 +0900

    確認画面のインプットの削除

--- frontend/src/features/eventPool/components/EventFormComponentsConfirmation.tsx
+++ frontend/src/features/eventPool/components/EventFormComponentsConfirmation.tsx
@@ -1,10 +1,6 @@
 "use client";
-import clsx from "clsx";
 import React, { useEffect, useState } from "react";
-import { InputWithLabel } from "~/components/common/InputWithLabel";
-import { WithLabel } from "~/components/common/WithLabel";
 import { Button } from "~/components/ui/button";
-import { Textarea } from "~/components/ui/textarea";
 import { BudgetMode } from "~/models/types/common";
 
 interface EventFormComponentsConfirmationProps {
@@ -48,97 +44,49 @@ export default function EventFormComponentsConfirmation({
   }, [name]);
 
   return (
-    <div className="flex flex-col">
-      <h2 className="text-lg font-bold mb-4">確認画面</h2>
-      <InputWithLabel
-        label="名前"
-        name="name"
-        id="name"
-        value={name}
-        readOnly
-        className={clsx(isTitle ? "" : "border-destructive")}
-      />
-      <p className="text-sm text-destructive">
-        イベントを作るのに名前は必要です。
-      </p>
-      <InputWithLabel
-        label="説明"
-        name="description"
-        id="description"
-        value={description}
-        readOnly
-      />
-      <InputWithLabel
-        label="場所"
-        name="location"
-        id="location"
-        value={location}
-        readOnly
-      />
-      <p className="font-bold text-lg mt-2">参加できる時間</p>
-      <WithLabel label="開始日時">
-        <p className="py-2">{startDateTime.toLocaleString()}</p>
-      </WithLabel>
-      <WithLabel label="終了日時">
-        <p className="py-2">{endDateTime.toLocaleString()}</p>
-      </WithLabel>
-      <InputWithLabel
-        label="所要時間(分)"
-        name="time"
-        id="time"
-        type="number"
-        value={duration}
-        readOnly
-      />
-      <div className="flex w-full gap-2">
-        <WithLabel label="1人あたり/合算">
-          <p className="py-2">
-            {budgetType === "per_person" ? "1人あたり" : "合算"}
-          </p>
-        </WithLabel>
-        <InputWithLabel
-          label="予算(円)"
-          name="value"
-          id="value"
-          type="number"
-          value={budget}
-          readOnly
-        />
-      </div>
-      <div className="flex items-center gap-2 mt-4">
-        <label className="text-sm font-medium leading-none">
-          予定を確定するまでにやることがある:
-        </label>
-        <p>{isPreparationChecked ? "はい" : "いいえ"}</p>
-      </div>
-      {isPreparationChecked && (
-        <InputWithLabel
-          label="予定の準備タスク"
-          name="preparation"
-          id="preparation"
-          value={preparationDetails}
-          readOnly
-        />
-      )}
-      <InputWithLabel
-        label="最大人数"
-        name="participants"
-        id="participants"
-        type="number"
-        value={participants}
-        readOnly
-      />
-      <WithLabel label="メモ">
-        <Textarea
-          name="memo"
-          id="memo"
-          className="h-24"
-          value={memo}
-          readOnly
-        />
-      </WithLabel>
+    <div className="flex flex-col gap-4">
+      <h2 className="text-sm font-bold">名前</h2>
+      <p className="border-destructive">{name ? name : "未設定"}</p>
 
-      <div className="flex flex-row justify-between p-4 gap-3">
+      <h2 className="text-sm font-bold">説明</h2>
+      <p>{description ? description : "未設定"}</p>
+
+      <h2 className="text-sm font-bold">場所</h2>
+      <p>{location ? location : "未設定"}</p>
+
+      <div className="flex flex-row gap-4">
+        <div className="flex flex-col">
+          <h2 className="text-sm font-bold">開始日時</h2>
+          <p className="py-2">{startDateTime.toLocaleString()}</p>
+        </div>
+        <div className="flex flex-col">
+          <h2 className="text-sm font-bold">終了日時</h2>
+          <p className="py-2">{endDateTime.toLocaleString()}</p>
+        </div>
+      </div>
+      <h2 className="text-sm font-bold">所要時間(分)</h2>
+      <p>{duration ? duration : "未設定"}</p>
+
+      <h2 className="text-sm font-bold">1人あたり/合算</h2>
+      <p>{budgetType === "per_person" ? "1人あたり" : "合算"}</p>
+
+      <h2 className="text-sm font-bold">予算</h2>
+      <p>{budget ? budget + "円" : "未設定"}</p>
+
+      {isPreparationChecked && (
+        <>
+          <h2 className="text-sm font-bold">参加への準備タスク</h2>
+          <p>{preparationDetails ? preparationDetails : "未設定"}</p>
+        </>
+      )}
+
+      <h2 className="text-sm font-bold">最大人数</h2>
+      <p>{participants ? participants : "未設定"}</p>
+
+      <h2 className="text-sm font-bold">メモ</h2>
+      <p>{memo ? memo : "未設定"}</p>
+
+      <div className="flex flex-row justify-between  gap-3">
         <Button onClick={onEdit} className="flex w-full">
           編集
         </Button>
